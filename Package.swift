b3a10d3fa52172a4f98403b31e2997f9f9b6de5e
Pedro <pedro@pepicrft.me>
2024-12-17 08:57:02 +0100

    Use SPM as a build tool

---
+++ Package.swift
@@ -0,0 +1,52 @@
+// swift-tools-version: 5.8.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "XCBBuildServiceProxyKit",
+    platforms: [.macOS("12.0")],
+    products: [
+        .library(
+            name: "XCBBuildServiceProxyKit",
+            type: .static,
+            targets: ["XCBBuildServiceProxyKit"]
+        ),
+        .library(
+            name: "MessagePack",
+            type: .static,
+            targets: ["MessagePack"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-log", .upToNextMajor(from: "1.4.2")),
+        .package(url: "https://github.com/apple/swift-nio", .upToNextMajor(from: "2.40.0"))
+    ],
+    targets: [
+        .target(
+            name: "XCBBuildServiceProxyKit",
+            dependencies: [
+                "MessagePack",
+                "XCBProtocol",
+                .product(name: "Logging", package: "swift-log")
+            ]
+        ),
+        .target(
+            name: "XCBProtocol",
+            dependencies: [
+                .product(name: "NIO", package: "swift-nio")
+            ]
+        ),
+        .target(
+            name: "MessagePack",
+            dependencies: [
+            ]
+        ),
+        .testTarget(
+            name: "MessagePackTests",
+            dependencies: [
+                "MessagePack",
+            ]
+        ),
+    ]
+)
